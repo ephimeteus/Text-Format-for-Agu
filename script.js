b88558347413ab3033cd47a6f1b589deb1a10da5
ephimeteus <109197865+ephimeteus@users.noreply.github.com>
2023-07-23 20:57:05 -0300

    Splitted the formating funct, separating the logic

--- script.js
+++ script.js
@@ -1,37 +1,29 @@
-function formatThis() {
-    let rawData = document.getElementById("textbox").value.trim();
+function extractString(rawData) {
     const data = rawData.split(",");
-    const mensaje = `${data[0]} - ${data[1]}
-    *Desde* ${data[2]} *a* ${data[3]}
-    *Pax* ${data[4]} *x* ${data[5]}
-              *Cobrar* ${data[6]}
-    Observaciones: ${data[7]}`;
-    document.querySelector("#text").value = mensaje; //HTML  
-    
+    const mensaje = 
+      `${data[0]} - ${data[1]}
+      *Desde* ${data[2]} *a* ${data[3]}
+      *Pax* ${data[4]} *x* ${data[5]}
+      *Cobrar* ${data[6]}
+      Observaciones: ${data[7]}`;
+    return mensaje;
 }
 
-
+function formatArray() {
+    let rawData = document.getElementById("textbox").value.trim();
+    document.querySelector("#text").value = extractString(rawData); //HTML  
+}
 
 function copyToClipboard() {
     // Get the text field
     var copyText = document.getElementById("text");
-  
-    // Select the text field
+      // Select the text field
     copyText.select();
     copyText.setSelectionRange(0, 99999); // For mobile devices
-  
-     // Copy the text inside the text field
+       // Copy the text inside the text field
     navigator.clipboard.writeText(copyText.value);
-  
-    // Alert the copied text
+      // Alert the copied text
     alert("Copied the text: " + copyText.value);
-  }
-
-// 1 - 2
-// *Desde* 3 *a* 4
-// *Pax* 5 *x* 6
-// *Cobrar*7
-
-// Observaciones: 8
+ }
 
 
